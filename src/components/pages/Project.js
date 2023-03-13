7b42c561318d8e415f662f77cb63bc59668a573e
logarythm <mahmood.ihsan98989@gmail.com>
2023-03-13 11:12:46 +0000

    created new file for projects and moved project gallery code to it

---
+++ src/components/pages/Project.js
@@ -0,0 +1,24 @@
+function Project(props) {
+    return (
+      <div className="card">
+        <div className="img-container">
+          <img alt={props.projectName} src={props.image} />
+        </div>
+        <div className="content">
+          <ul>
+            <li>
+              <strong></strong> {props.projectName}
+            </li>
+            <li>
+              <strong>Repo</strong> {props.githubrepo}
+            </li>
+            <li>
+              <strong>Pages</strong> {props.githubpages}
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Project
