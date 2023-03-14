5968f521bb42051e2f6198704257142fb2f5fccd
logarythm <mahmood.ihsan98989@gmail.com>
2023-03-14 16:18:26 +0000

    cleaned up code

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -7,26 +7,7 @@ function Project(props) {
   
     return (
     
-      //       <div className="col-sm-12 col-lg-6 main-card">
-      //   <div className="card">
-      //   <div className="img-container">
-      //     <img className=" project-img" alt={props.projectName} src={require(`../../assets/images/`+props.image)} />
-      //   </div>
-      //   <div className="content">
-      //     <ul>
-      //       <li>
-      //       {props.projectName}
-      //       </li>
-      //       <li>
-      //    {props.githubrepo}
-      //       </li>
-      //       <li>
-      //         {props.githubpages}
-      //       </li>
-      //     </ul>
-      //   </div>
-      // </div>
-      // </div>
+      
       <div className="card project-card">
   <img className="card-img-top" src={require(`../../assets/images/`+props.image)} alt={props.projectName}/>
   <div className="card-body">
