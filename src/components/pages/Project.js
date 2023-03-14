48d4c99070181afb3928922a21058031f3c86790
logarythm <mahmood.ihsan98989@gmail.com>
2023-03-14 20:44:56 +0000

    changed class names

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -11,8 +11,8 @@ function Project(props) {
       <div className="card project-card">
   <img className="card-img-top" src={require(`../../assets/images/`+props.image)} alt={props.projectName}/>
   <div className="card-body">
-    <h5 className="card-title about-text">{props.projectName}</h5>
-    <p className="card-text about-text">{props.description}</p>
+    <h5 className="card-title text">{props.projectName}</h5>
+    <p className="card-text text">{props.description}</p>
     <a href={props.githubrepo} className="btn btn-info">Repository</a>
     <a href={props.githubpages} className="btn btn-dark">Deployed</a>
   </div>
